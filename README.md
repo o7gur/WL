ab30a64b63d00cd812207a6b0ad56346c377eea2
hegde-atri <iamatrihegde@gmail.com>
2022-04-12 11:53:53 +0100

    done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WL
-weight logger through shell script
+
+Logs entered weight to specified file. Use `-h` flag for help
+
+## Usage
+
+Download the repo and use `chmod +x wl.sh` to make it executable. You can now run it using `./wl.sh -h`.
+
+You can now set it an alias in your .bashrc file (bash users) or your fish config file (fish users).
--- wl.sh
+++ wl.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/bash
-#Allows you to log daily weight with timestamps (morning and night) through the console
-echo -n "Enter Weight: "
-read Weight
+# define path to file here
+path=~/Documents/obsidian-vaults/personlogs/LOGS/weight.md
 
-currentDate=`date +"%d/%m/%Y"`
-currentTime=`date +"%T"`
-
-echo "| $currentDate | $currentTime | $Weight |" >> ~/Documents/obsidian-vaults/personlogs/LOGS/weight.md
-echo -n "Recorded Entry"
+if [ "$1" == "-m" ]; then
+  weight=$2
+  currentTime=`date +"%T"`
+  echo "m - $weight ($currentTime)" >> $path
+elif [ "$1" == "-n" ]; then
+  weight=$2
+  currentTime=`date +"%T"`
+  echo "n - $weight ($currentTime)" >> $path
+elif [ "$1" == "-d" ]; then
+  currentDate=`date +"%d/%m/%Y"`
+  echo "" >> $path
+  echo "\`$currentDate\`" >> $path
+elif [ "$1" == "-h" ]; then
+  echo "-----------------------------------------"
+  echo "|        use -d to set new date         |"
+  echo "|  use -m 56 to add morning entry of 56 |"
+  echo "|   use -n 56 to add night entry of 56  |"
+  echo "-----------------------------------------"
+else
+  echo "------------------------------------------------------------"
+  echo "| incorrect usage, use flags [-m weight] [-n weight] [-d]  |";
+  echo "|                                                          |"
+  echo "|                 use -h flag for help                     |"
+  echo "------------------------------------------------------------"
+fi
